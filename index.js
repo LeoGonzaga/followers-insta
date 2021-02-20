6ce0a9402c4a118c2a3b33a3dd69e1e7e0947eb6
LeoGonzaga <sacredlotus05@gmail.com>
2021-02-19 23:26:40 -0300

    modificando o bot para buscar informações no instastatics

--- index.js
+++ index.js
@@ -3,34 +3,73 @@ const express = require("express");
 const app = express();
 const port = 3000;
 let followers;
-async function getFollowrs() {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-  await page.goto("https://www.instagram.com/matheusyuji.s/?hl=pt-br");
+let followers2;
 
-  await page.waitForSelector(".g47SY");
+async function getFollowrs() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  // await page.goto("https://www.instagram.com/leogonzaga__/?hl=pt-br");
+  // await page.waitForSelector(".g47SY");
+
+  await page.goto("https://instastatistics.com/#!/projota");
+  await page.waitForSelector(".odometer-digit");
+
+  const list = await page.evaluate(() => {
+    let followersArray = [];
+    let string = "";
+
+    // const nodeList = document.querySelectorAll(".g47SY");
+    const nodeList = document.querySelectorAll(".odometer-digit");
+    const arrayInfo = [...nodeList];
+    console.log(arrayInfo);
+    arrayInfo.map((i) => {
+      console.log("Texto", i.innerText);
+      string += i.innerText;
+      followersArray.push(i.innerHTML);
+    });
+    console.log("array", string);
+
+    return string;
+  });
+  followers = list;
+  console.log("Projota", list);
+  await getFollowrs2();
+}
+
+async function getFollowrs2() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  // await page.goto("https://www.instagram.com/leogonzaga__/?hl=pt-br");
+  // await page.waitForSelector(".g47SY");
+
+  await page.goto("https://instastatistics.com/#!/karolconka");
+  await page.waitForSelector(".odometer-digit");
+
   console.log("entrou aqui");
   const list = await page.evaluate(() => {
     let followersArray = [];
+    let string = "";
 
-    const nodeList = document.querySelectorAll(".g47SY");
+    // const nodeList = document.querySelectorAll(".g47SY");
+    const nodeList = document.querySelectorAll(".odometer-digit");
     const arrayInfo = [...nodeList];
     console.log(arrayInfo);
-    const list = arrayInfo.map((i) => {
-      console.log(i.innerText);
+    arrayInfo.map((i) => {
+      console.log("Texto", i.innerText);
+      string += i.innerText;
       followersArray.push(i.innerHTML);
     });
-    console.log(followersArray);
+    console.log("array", string);
 
-    return followersArray;
+    return string;
   });
-  followers = list[1];
-  console.log(list);
+  followers2 = list;
+  console.log("Karol", list);
 }
 
 app.get("/", async (req, res) => {
   await getFollowrs();
-  res.send({ Seguidores: followers });
+  res.send({ Seguidores: { Projota: followers, Karol: followers2 } });
 });
 
 app.listen(port, () => {
